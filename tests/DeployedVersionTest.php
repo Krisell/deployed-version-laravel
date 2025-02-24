61aebf9c127403d9988de45e2c66e8503036fbc5
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-02-24 16:19:36 +0100

    Update dependencies

--- tests/DeployedVersionTest.php
+++ tests/DeployedVersionTest.php
@@ -3,6 +3,7 @@
 namespace Krisell\DeployedVersionLaravel\Tests;
 
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class DeployedVersionTest extends TestCase
 {
@@ -11,19 +12,19 @@ class DeployedVersionTest extends TestCase
         return ['Krisell\DeployedVersionLaravel\DeployedVersionServiceProvider'];
     }
 
-    /** @test */
+    #[Test]
     public function the_version_route_is_added_and_uses_prefix_set_in_env()
     {
         $this->get('dev-custom/version')->assertStatus(200);
     }
 
-    /** @test */
+    #[Test]
     public function the_default_version_is_TESTING_VALUE()
     {
         $this->get('dev-custom/version')->assertJson(['version' => 'TESTING_VALUE']);
     }
 
-    /** @test */
+    #[Test]
     public function the_version_can_be_set_in_the_environment()
     {
         config(['deployed-version.version' => 'NEW_TESTING_VALUE']);
